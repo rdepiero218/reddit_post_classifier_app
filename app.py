32e74671fa0e717eef5e880c9f9ad0d88b6e1501
Regene DePiero <regenedepiero@gmail.com>
2025-04-12 17:01:09 -0400

    updated to use container width in images

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ st.header('Fallout vs Star Trek')
 ### dispaly logo images
 col1, col2 = st.columns(2)
 with col1:
-    st.image('./images/fallout-logo.png', use_column_width=True)
+    st.image('./images/fallout-logo.png', use_container_width=True)
 with col2:
-    st.image('./images/startrek-logo.png', use_column_width=True)
+    st.image('./images/startrek-logo.png', use_container_width=True)
 
 ### explanation of app
 st.subheader('What does it do?')
